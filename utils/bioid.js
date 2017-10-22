e339684b1758d4c1366a8800fd21a074c64d25ee
Daniel Andrews <danielandrewsre@gmail.com>
2017-10-22 01:45:06 -0700

    this time it was actually krishnan. dont employ hi

--- utils/bioid.js
+++ utils/bioid.js
@@ -26,10 +26,11 @@ function get_face(params) {
     return new Promise(function(resolve, reject) {
         rekognition.searchFacesByImage(params, function(err, data) {
             if (err) reject(err);
-            if (!data || !data.FaceMatches || data.FaceMatches.length == 0 || !data.FaceMatches[0].Face || !data.FaceMatches[0].Face.FaceId) {
-                reject(err);
-            }
-            resolve(data.FaceMatches[0].Face.FaceId);
+            console.log(data);
+            if (!data || !data.FaceMatches || data.FaceMatches.length == 0 || !data.FaceMatches[0].Face || !data.FaceMatches[0].Face.FaceId) 
+                reject(err); 
+            else 
+                resolve(data.FaceMatches[0].Face.FaceId);
         });
     });
 }
